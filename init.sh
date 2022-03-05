120bbffd0c63e3ae4c65b05c140974329e5d3b25
Shivam Parikh <sparikh@berkeley.edu>
2022-03-04 16:53:33 -0800

    changes to init

--- init.sh
+++ init.sh
@@ -2,13 +2,9 @@
 
 echo "Initializing the solana_nft project"
 echo "..."
-if ! [ -x "$(command -v solana)" ]; then
-    echo 'Error: solana is not installed.' >&2
-    echo "Installing solana"
-    sh -c "$(curl -sSfL https://release.solana.com/v1.9.9/install)"
-else
-    echo "solana is already installed"
-fi
+echo "Installing solana"
+sh -c "$(curl -sSfL https://release.solana.com/v1.9.9/install)"
+
 python3 -m virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
